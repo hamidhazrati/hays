7eb8dd01491e0369cedc3faca4d2322497558bc2
hamidhazrati <hamid.hazrati@hmcts.net>
2022-02-10 09:21:47 +0000

    updated functional routes in unit test

--- src/app/acme-dashboard/acme-dashboard.component.spec.ts
+++ src/app/acme-dashboard/acme-dashboard.component.spec.ts
@@ -22,11 +22,61 @@ describe('AcmeDashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AcmeDashboardComponent);
     component = fixture.componentInstance;
+    component.data = [
+      {
+        id: 1,
+        quetion: 'a',
+        answer: 'b',
+        open: false,
+      },
+      {
+        id: 2,
+        quetion: 'c',
+        answer: 'd',
+        open: false,
+      },
+      {
+        id: 3,
+        quetion: 'e',
+        answer: 'f',
+        open: false,
+      },
+    ]
     fixture.detectChanges();
-    // component.start();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initially accordion be all collapse', () => {
+    expect(component.data[0].open || component.data[1].open || component.data[2].open).toBeFalsy();
+  });
+  it('should open second row', () => {
+    component.data[0].open = true;
+    component.data[1].open = true;
+    component.data[2].open = true;
+    component.closeAllRows()
+    expect(component.data[0].open || component.data[1].open || component.data[2].open).toBeFalsy();
+  });
+
+  it('should on row click open row and close others', () => {
+    component.onRowOpened(component.data[1]);
+    expect(component.data[0].open || component.data[2].open).toBeFalsy();
+    expect(component.data[1].open).toBeTruthy();
+  });
+
+  it('should on an open row click close row then all others should be closed', () => {
+    component.data[1].open = true;
+    component.onRowClosed(component.data[1]);
+    expect(component.data[0].open || component.data[1].open || component.data[2].open).toBeFalsy();
+  });
+
+  it('should on an open row , open another row then all others should be closed apart from new open ', () => {
+    component.data[1].open = true;
+    component.onRowOpened(component.data[0]);
+    expect(component.data[1].open || component.data[2].open).toBeFalsy();
+    expect(component.data[0].open).toBeTruthy();
+  });
+
 });
